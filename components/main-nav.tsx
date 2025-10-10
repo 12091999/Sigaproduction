080ff6ffc0cedf4859f053a119bb5bb79c8fe027
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2025-10-10 21:41:49 +0700

    initial commit

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -83,7 +83,7 @@ export function MainNav() {
 
     const secondaryRoutes: Route[] = [ 
     {
-      href: "/components/about",
+      href: "/about",
       label: "About",
       active: pathname === "/about",
     },
