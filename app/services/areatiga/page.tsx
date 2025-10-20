8233a84147d06a4d464ffecb5393ad3e32af0011
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2025-10-20 20:35:50 +0700

    first commit

--- app/services/areatiga/page.tsx
+++ app/services/areatiga/page.tsx
@@ -9,7 +9,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Separator } from "@/components/ui/separator"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import HeroSection from "@/components/hero-sectionEO"
+import HeroSection from "@/components/hero-sectionA"
 
 export const metadata: Metadata = {
   title: "AREATIGA - AREATIGA",
