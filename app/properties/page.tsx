9b53d068ab525fd89941af91cd75859cb07c6c97
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2025-10-22 18:54:08 +0700

    first commit

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -156,7 +156,6 @@ export default function PropertiesPage() {
             <TabsList>
               <TabsTrigger value="all">All</TabsTrigger>
               <TabsTrigger value="rent">For Rent</TabsTrigger>
-              <TabsTrigger value="sale">For Sale</TabsTrigger>
             </TabsList>
             <TabsContent value="all" className="mt-6">
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
