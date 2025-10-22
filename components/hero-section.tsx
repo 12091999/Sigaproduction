894bc0fe9c5078842012be503a3ee5755ff5bd90
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2025-10-22 18:33:14 +0700

    first commit

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -78,7 +78,7 @@ export default function HeroSection() {
               <SelectContent>
                 <SelectItem value="all">All Categories</SelectItem>
                 <SelectItem value="Studio 3 Music Studio">Studio 3 Music Studio</SelectItem>
-                <SelectItem value="EO">Sigapro EO</SelectItem>
+                <SelectItem value="sigaproEO">Sigapro EO</SelectItem>
                 <SelectItem value="Sigamerch">Sigamerch</SelectItem>
                 <SelectItem value="Sigma Bwx">Sigma Bwx</SelectItem>
                 <SelectItem value="Area Tiga">Area Tiga</SelectItem>
