3962bbd725c8a84f1d0b1f84e62ce391bcf11b15
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2025-10-23 19:03:29 +0700

    first commit

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -162,7 +162,7 @@ export default function PropertiesPage() {
             <TabsContent value="all" className="mt-6">
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {properties.map((property) => (
-                  <PropertyCard key={property.id} property={property} />
+                  <PropertyCard key={property.slug} property={property} />
                 ))}
               </div>
             </TabsContent>
@@ -171,7 +171,7 @@ export default function PropertiesPage() {
                 {properties
                   .filter((property) => property.type === "Rent")
                   .map((property) => (
-                    <PropertyCard key={property.id} property={property} />
+                    <PropertyCard key={property.slug} property={property} />
                   ))}
               </div>
             </TabsContent>
@@ -180,7 +180,7 @@ export default function PropertiesPage() {
                 {properties
                   .filter((property) => property.type === "Rent")
                   .map((property) => (
-                    <PropertyCard key={property.id} property={property} />
+                    <PropertyCard key={property.slug} property={property} />
                   ))}
               </div>
             </TabsContent>
