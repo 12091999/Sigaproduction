9fc977217683947adcc67f65feb7709d9c883930
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2025-11-02 10:18:22 +0700

    Initial commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import { ShoppingBag, Car, Hotel, Map, Plus } from "lucide-react"
+import { ShoppingBag, Book, Home, Plus } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
@@ -82,7 +82,7 @@ export default function DashboardPage() {
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">Services</CardTitle>
-                <Car className="h-4 w-4 text-muted-foreground" />
+                <Book className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">3</div>
@@ -93,7 +93,7 @@ export default function DashboardPage() {
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">Rent</CardTitle>
-                <Hotel className="h-4 w-4 text-muted-foreground" />
+                <Home className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">5</div>
