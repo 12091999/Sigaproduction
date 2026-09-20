c71b107b9e7b8a0d8f93e848a4230316832fddb5
Muhammad Fajar Kurniawan <muhammadfajar12091999@gmail.com>
2026-09-20 14:31:13 +0700

    Auto update

--- app/properties/Gitar/page.tsx
+++ app/properties/Gitar/page.tsx
@@ -146,7 +146,7 @@ export default function RentInstrumentPage() {
           {/* IMAGE SLIDER */}
           <div className="relative aspect-video rounded-lg overflow-hidden">
             <Image
-              src={instrument.images[currentImageIndex] || "/images/gitar1.jpg"}
+              src={instrument.images[currentImageIndex] || "/placeholder.svg"}
               alt={`Instrument image ${currentImageIndex + 1}`}
               fill
               className="object-cover"
